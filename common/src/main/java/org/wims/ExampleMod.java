7ef59c41fb7114b0f88c2e2fd1670a3bba9310a7
Aaron Corson <chaopivoo@live.com>
2026-02-14 14:07:47 -0800

    initial commit

---
+++ common/src/main/java/org/wims/ExampleMod.java
@@ -0,0 +1,9 @@
+package org.wims;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "wims";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
