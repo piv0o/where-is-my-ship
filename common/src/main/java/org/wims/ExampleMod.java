c52b2274170dbdaa951b13498d8ecd409ff2a735
Aaron Corson <chaopivoo@live.com>
2026-02-15 12:01:08 -0800

    Swapped to VS Template to get out of dependency hell

--- common/src/main/java/org/wims/ExampleMod.java
+++
@@ -1,9 +0,0 @@
-package org.wims;
-
-public final class ExampleMod {
-    public static final String MOD_ID = "wims";
-
-    public static void init() {
-        // Write common init code here.
-    }
-}
